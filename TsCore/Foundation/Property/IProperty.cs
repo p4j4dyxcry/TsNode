c16553253862d05d708b0405240cd3aafc5a5544
yuki.tsuneyama <at12k313@gmail.com>
2020-03-14 10:04:28 +0900

    Exampleを別repositoryへ移動

--- TsCore/Foundation/Property/IProperty.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TsCore.Foundation.Property
-{
-    public interface IProperty
-    {
-        string Name { get; set; }
-
-        object Data { get; set; }
-    }
-}
--- TsCore/Operation/Internal/ErrorMessages.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TsCore.Operation.Internal
-{
-    internal static class ErrorMessages
-    {
-        public static string InvalidOperation { get; } = "無効な呼び出しです。";
-        public static string NotNull { get; } = "nullは許容されません。";
-    }
-}
