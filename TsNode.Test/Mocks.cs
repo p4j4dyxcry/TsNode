493a0a04b89db827cd32138eed471aef71c3fdf6
yuki.tsuneyama <yuki.tsuneyama@siliconstudio.co.jp>
2020-03-18 20:38:40 +0900

    add drag tests

---
+++ TsNode.Test/Mocks.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+using TsNode.Controls;
+using TsNode.Interface;
+
+namespace TsNode.Test
+{
+    public class TestNodeContext : INodeDataContext
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public bool IsSelected { get; set; }
+        public bool TryConnect(ConnectInfo connectInfo)
+        {
+            return true;
+        }
+
+        public double X { get; set; }
+        public double Y { get; set; }
+        
+        public IList<IPlugDataContext> InputPlugs { get; } = new List<IPlugDataContext>();
+        public IList<IPlugDataContext> OutputPlugs { get; } = new List<IPlugDataContext>();
+
+        public IEnumerable<IPlugDataContext> GetInputPlugs() => InputPlugs;
+
+
+        public IEnumerable<IPlugDataContext> GetOutputPlugs() => OutputPlugs;
+    }
+    
+    public class TestNodeControl : INodeControl , ISelectable
+    {
+        public double X 
+        { 
+            get => _dataContext.X; 
+            set => _dataContext.X = value; 
+        }
+        public double Y 
+        { 
+            get => _dataContext.Y; 
+            set => _dataContext.Y = value; 
+        }
+
+        public bool IsMouseOver { get; set; }
+        public object DataContext => _dataContext;
+        public double ActualWidth { get; set; }
+        public double ActualHeight { get; set; }
+
+        public event Action<object, UpdateNodePointArgs> UpdatePoints;
+        
+        public IList<IPlugControl> OutPutPlugs { get; } = new List<IPlugControl>();
+        public IList<IPlugControl> InPutPlugs { get; } = new List<IPlugControl>();
+
+        public IEnumerable<IPlugControl> GetOutputPlugs() => OutPutPlugs;
+
+        public IEnumerable<IPlugControl> GetInputPlugs() => InPutPlugs;
+
+        public bool IsSelected
+        {
+            get => _dataContext.IsSelected;
+            set => _dataContext.IsSelected = value;
+        }
+
+        private readonly INodeDataContext _dataContext;
+
+        public TestNodeControl(INodeDataContext nodeDataContext)
+        {
+            _dataContext = nodeDataContext;
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action<object> _delegate;
+
+        public DelegateCommand(Action<object> @delegate)
+        {
+            _delegate = @delegate;
+        }
+        
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _delegate(parameter);
+        }
+
+        public void RaiseCanExecute()
+        {
+            CanExecuteChanged?.Invoke(this,EventArgs.Empty);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+    
+    public static class Mock
+    {
+        public static TestNodeControl CreateNodeControl(double x, double y, bool isSelected = false)
+        {
+            return new TestNodeControl(new TestNodeContext()
+            {
+                X = x,
+                Y = y,
+                IsSelected = isSelected,
+            });
+        }
+
+        public static ICommand Command<T>(Action<T> action)
+        {
+            return new DelegateCommand((x) => action((T) x));
+        }
+        
+        public static ICommand Command(Action action)
+        {
+            return new DelegateCommand((x) => action());
+        }
+    }
+}
---
+++ TsNode.Test/NodesDrag.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using TsNode.Controls;
+using TsNode.Controls.Connection;
+using TsNode.Controls.Drag;
+using TsNode.Controls.Drag.Controller;
+using TsNode.Interface;
+using Xunit;
+
+namespace TsNode.Test
+{
+    public class NodesDrag
+    {
+        [Fact]
+        public void DragSingleNodeTest()
+        {
+            var nodes = new INodeControl[]
+            {
+                Mock.CreateNodeControl(0,50,true),
+            };
+
+            var executedCompleteCommand = false;
+            var nodeDragCompletedCommand = Mock.Command<CompletedMoveNodeEventArgs>(x =>
+            {
+                AssertCheckNodeMovePoint(x, nodes[0], 0, 50, 10, 60);
+
+                executedCompleteCommand = true;
+            });
+
+            var controller = BuildController(nodes, nodeDragCompletedCommand);
+
+            DoDrag( controller , new Point(10,10) );
+            
+            controller.OnDragEnd();
+            
+            Assert.True(executedCompleteCommand);
+        }
+
+        [Fact]
+        public void DragMultiNodeTest()
+        {
+            var nodes = new INodeControl[]
+            {
+                Mock.CreateNodeControl(-20,0,true),
+                Mock.CreateNodeControl(0,0,false),
+                Mock.CreateNodeControl(10,-50,true),
+            };
+
+            var executedCompleteCommand = false;
+            var nodeDragCompletedCommand = Mock.Command<CompletedMoveNodeEventArgs>(x =>
+            {
+                // selected が falseなものはドラッグされていない
+                Assert.Equal(2,x.InitialNodePoints.Count);
+
+                Assert.Equal(0,nodes[1].X);
+                Assert.Equal(0,nodes[1].Y);
+                
+                AssertCheckNodeMovePoint(x, nodes[0], -20, 0, -10, 10);
+                
+                AssertCheckNodeMovePoint(x, nodes[2], 10, -50, 20, -40);
+
+                executedCompleteCommand = true;
+            });
+
+            var controller = BuildController(nodes, nodeDragCompletedCommand);
+
+            DoDrag( controller , new Point(10,10));
+            controller.OnDragEnd();
+            
+            Assert.True(executedCompleteCommand);
+        }
+
+        [Fact]
+        public void ChainDragTest()
+        {
+            var nodes = new INodeControl[]
+            {
+                Mock.CreateNodeControl(0,0,true),
+            };
+
+            var executedCompleteCommand = false;
+            var nodeDragCompletedCommand = Mock.Command<CompletedMoveNodeEventArgs>(x =>
+            {
+                AssertCheckNodeMovePoint(x, nodes[0], 0, 0, 30, 0);
+
+                executedCompleteCommand = true;
+            });
+
+            var controller = BuildController(nodes, nodeDragCompletedCommand);
+
+            // 10,10 → -5,-5 → 30,0 へ順番に動かす
+            DoDrag( controller , new Point(10,10) , new Point(-5,-5) , new Point(30,0));
+            controller.OnDragEnd();
+            
+            Assert.True(executedCompleteCommand);
+        }
+        
+        [Fact]
+        public void FailedTest()
+        {
+            var nodes = new INodeControl[]
+            {
+                Mock.CreateNodeControl(0,0,false),
+            };
+
+            var controller = BuildController(nodes, null);
+            
+            Assert.Null(controller);
+        }
+
+        /// <summary>
+        /// ドラッグコントローラを作成する
+        /// </summary>
+        /// <param name="nodes"></param>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        private IDragController BuildController(INodeControl[] nodes , ICommand command)
+        {
+            var builder = new DragControllerBuilder(null, MouseButton.Left, nodes, Array.Empty<ConnectionShape>());
+            var controller = builder
+                .AddBuildTarget(new NodesDragBuild(builder, 1, false,0))
+                .SetNodeDragControllerBuilder(command)
+                .Build();
+
+            return controller;
+        }
+
+        /// <summary>
+        /// ノード座標をチェック
+        /// </summary>
+        /// <param name="args"></param>
+        /// <param name="node"></param>
+        /// <param name="originX">元のx</param>
+        /// <param name="originY">元のy</param>
+        /// <param name="movedX">移動後のx</param>
+        /// <param name="movedY">移動後のy</param>
+        private void AssertCheckNodeMovePoint(CompletedMoveNodeEventArgs args , INodeControl node, double originX, double originY , double movedX , double movedY)
+        {
+            var prevPoint = args.InitialNodePoints[node.DataContext as INodeDataContext];
+            var endPoint  = args.CompletedNodePoints[node.DataContext as INodeDataContext];
+                
+            Assert.Equal(originX ,prevPoint.X);
+            Assert.Equal(originY,prevPoint.Y);
+                
+            Assert.Equal(movedX ,endPoint.X);
+            Assert.Equal(movedY,endPoint.Y);
+                
+            Assert.Equal(movedX ,node.X);
+            Assert.Equal(movedY, node.Y);
+        }
+
+        /// <summary>
+        /// ドラッグを開始
+        /// </summary>
+        /// <param name="controller">ドラッグコントローラ</param>
+        /// <param name="first">1回目のドラッグ位置</param>
+        /// <param name="others">2回目以降のドラッグ位置</param>
+        private void DoDrag(IDragController controller , Point first, params Point[] others)
+        {
+            // start drag
+            var startArgs = new DragControllerEventArgs(new Point(0,0),new Point(0,0),new Vector(0,0),MouseButton.Left);
+            {
+                Assert.True(controller.CanDragStart(startArgs));
+            
+                controller.OnStartDrag(startArgs);                
+            }
+
+            // drag moving
+            var firstDragArgs = startArgs.CreateUpdatedArgs(first);
+            {
+                controller.OnDragMoving(firstDragArgs);
+            }
+            
+            // other dragging
+            {
+                var args = firstDragArgs;
+                foreach (var other in others)
+                {
+                    args = args.CreateUpdatedArgs(other);
+                    controller.OnDragMoving(args);
+                }
+            }
+        }
+    }
+}
