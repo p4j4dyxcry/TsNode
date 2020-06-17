9420517157ec34f392f5f1780bf039b1881c852f
yuki.tsuneyama <at12k313@gmail.com>
2020-06-17 17:23:34 +0900

    add node engine sample

---
+++ NodeEngineSample/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using TsNode.Preset;
+
+namespace NodeEngineSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var engine = new NodeEngine();
+            DataContext = engine.BuildViewModel();
+
+            engine.GetOrCreateNode("Node1")
+                .SetPos(0, 0)
+                .SetColor(Colors.Red);
+
+            engine.GetOrCreateNode("Node2")
+                .SetPos(0, 100);
+            
+            engine.Connect("Node1" , "Node2");
+        }
+    }
+}
